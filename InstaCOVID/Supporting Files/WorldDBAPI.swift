8d8b6e7b65a0450a23d8b99b22d2ad15245548be
JiaruiLyu <lyujr616@vt.edu>
2020-11-20 19:22:00 -0500

    Update Supporting Files, User date, Delegates

---
+++ InstaCOVID/Supporting Files/WorldDBAPI.swift
@@ -0,0 +1,21 @@
+//
+//  WorldDBAPI.swift
+//  InstaCOVID
+//
+//  Created by Nicole Lyu on 11/20/20.
+//  Copyright © 2020 team2. All rights reserved.
+//
+
+import SwiftUI
+
+struct WorldDBAPI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WorldDBAPI_Previews: PreviewProvider {
+    static var previews: some View {
+        WorldDBAPI()
+    }
+}
