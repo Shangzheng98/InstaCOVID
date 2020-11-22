f28d591566f87d6a62477348facee6b84b41ba00
Tenghui Zhang <tenghuiz@vt.edu>
2020-11-22 15:27:08 -0500

    These are Test version

---
+++ InstaCOVID/SettingMain.swift
@@ -0,0 +1,21 @@
+//
+//  SettingMain.swift
+//  InstaCOVID
+//
+//  Created by Tenghui Zhang on 11/22/20.
+//  Copyright © 2020 team2. All rights reserved.
+//
+
+import SwiftUI
+
+struct SettingMain: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingMain_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingMain()
+    }
+}
