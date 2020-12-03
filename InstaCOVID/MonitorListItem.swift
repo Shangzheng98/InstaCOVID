36c0d00eac8f4b6a2fa801304d1616c85a5bf266
Ruichang Chen <chenrc@vt.edu>
2020-12-03 19:56:44 +0800

    Monitor Ver 1.0 Only Map

---
+++ InstaCOVID/MonitorListItem.swift
@@ -0,0 +1,21 @@
+//
+//  MonitorListItem.swift
+//  InstaCOVID
+//
+//  Created by Ruichang Chen on 2020/12/3.
+//  Copyright © 2020 team2. All rights reserved.
+//
+
+import SwiftUI
+
+struct MonitorListItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MonitorListItem_Previews: PreviewProvider {
+    static var previews: some View {
+        MonitorListItem()
+    }
+}
